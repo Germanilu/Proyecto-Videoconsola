fd6ec06b6bd466dc3488be01ff2ba32fca255100
Germanilu <lucianogermani93@gmail.com>
2022-05-15 19:03:55 +0200

    Agregado audio cuando se enciende la consola

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // Function para encender la consola.
 function myFunction() {
-    var textoGameboy = document.getElementById("gameboyText");
-    var textoNintendo = document.getElementById("nintendoText");
-    var led = document.getElementById("led");
+    let textoGameboy = document.getElementById("gameboyText");
+    let textoNintendo = document.getElementById("nintendoText");
+    let led = document.getElementById("led");
+    let audio = document.createElement("audio");
     // Condicion para hacer que aparezcan y desaparezcan los elementos al hacer click. 
     if (textoGameboy.style.display === "none" && textoNintendo.style.display === "none") {
         textoGameboy.style.display = "block";
         textoNintendo.style.display = "flex";
-        led.classList.add("led-animation")
+        led.classList.add("led-animation");
+        audio.setAttribute("src","/mp3/GameBoy-Audio-Sound.mp3");
+        audio.play();
     } else {
         textoGameboy.style.display = "none";
         textoNintendo.style.display = "none";
